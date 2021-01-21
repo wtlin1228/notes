b85d70de19ebb00ce3bf1410e096d2474fb3abdd
wtlin <wtlin1228@gmail.com>
2021-01-21 23:31:16 +0800

    gatsby-intro: add graphql

--- frontend-master/introduction-to-gatsby/gatsby-config.js
+++ frontend-master/introduction-to-gatsby/gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'UnaSees',
+    description: 'A site for UnaSees!',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 }
--- frontend-master/introduction-to-gatsby/src/components/Layout.js
+++ frontend-master/introduction-to-gatsby/src/components/Layout.js
@@ -1,69 +1,81 @@
 import React from 'react'
 import { Global, css } from '@emotion/react'
+import Helmet from 'react-helmet'
 import Header from './Header'
+import useSiteMetadata from '../hooks/useSiteMetadata'
 
-const Layout = ({ children }) => (
-  <>
-    <Global
-      styles={css`
-        * {
-          box-sizing: border-box;
-          margin: 0;
-        }
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata()
 
-        * + * {
-          margin-top: 1rem;
-        }
-
-        html,
-        body {
-          margin: 0;
-          color: #555;
-          font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-            Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-          font-size: 18px;
-          line-height: 1.4;
-
-          /* remove margin for the main div that Gatsby mounts into */
-          > div {
-            margin-top: 0;
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+            margin: 0;
           }
 
-          h1,
-          h2,
-          h3,
-          h4,
-          h5,
-          h6 {
-            color: #222;
-            line-height: 1.1;
+          * + * {
+            margin-top: 1rem;
+          }
 
-            + * {
-              margin-top: 0.5rem;
+          html,
+          body {
+            margin: 0;
+            color: #555;
+            font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+              sans-serif;
+            font-size: 18px;
+            line-height: 1.4;
+
+            /* remove margin for the main div that Gatsby mounts into */
+            > div {
+              margin-top: 0;
+            }
+
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6 {
+              color: #222;
+              line-height: 1.1;
+
+              + * {
+                margin-top: 0.5rem;
+              }
+            }
+
+            strong {
+              color: #222;
+            }
+
+            li {
+              margin-top: 0.25rem;
             }
           }
-
-          strong {
-            color: #222;
-          }
-
-          li {
-            margin-top: 0.25rem;
-          }
-        }
-      `}
-    />
-    <Header />
-    <main
-      css={css`
-        margin: 2rem auto 4rem;
-        max-width: 90vw;
-        width: 550px;
-      `}
-    >
-      {children}
-    </main>
-  </>
-)
+        `}
+      />
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Header />
+      <main
+        css={css`
+          margin: 2rem auto 4rem;
+          max-width: 90vw;
+          width: 550px;
+        `}
+      >
+        {children}
+      </main>
+    </>
+  )
+}
 
 export default Layout
