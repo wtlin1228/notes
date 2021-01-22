7c5c86522507c018f31e894d5306e32196133eb8
wtlin <wtlin1228@gmail.com>
2021-01-23 00:37:43 +0800

    gatsby-intro: analyze

--- frontend-master/introduction-to-gatsby/gatsby-config.js
+++ frontend-master/introduction-to-gatsby/gatsby-config.js
@@ -37,5 +37,14 @@ module.exports = {
         username: '2295436744',
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 }
