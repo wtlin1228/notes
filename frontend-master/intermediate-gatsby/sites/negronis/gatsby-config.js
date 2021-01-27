b1bd20a25a5c7a2dde1f14160539cf52f3f174b8
wtlin <wtlin1228@gmail.com>
2021-01-28 06:25:16 +0800

    intermediate gatsby: theme composition

--- frontend-master/intermediate-gatsby/sites/negronis/gatsby-config.js
+++ frontend-master/intermediate-gatsby/sites/negronis/gatsby-config.js
@@ -28,12 +28,12 @@ module.exports = {
         contentPath: 'content/recipes',
       },
     },
-    // {
-    //   resolve: '@jlengstorf/gatsby-theme-events',
-    //   options: {
-    //     basePath: '/events',
-    //     contentPath: 'content/events',
-    //   },
-    // },
+    {
+      resolve: '@jlengstorf/gatsby-theme-events',
+      options: {
+        basePath: '/events',
+        contentPath: 'content/events',
+      },
+    },
   ],
 };
